cee1b2117eeb7ec06cf6380cc6703cffe74f7433
eternaldream799@gmail.com <200289ur>
2022-02-15 10:07:59 +0600

    Changed

--- src/com/company/Class.java
+++ src/com/company/Class.java
@@ -4,22 +4,13 @@ import java.text.DecimalFormat;
 
 public class Class {
     private final static float PI = 3.1415f;
-    private static float radius;
 
-    public static void area() {
+    public static void area(float radius, float radius1) {
         float area = PI * (radius * radius);
         System.out.println("Area: " + area);
     }
-    public static void circumference() {
+    public static void circumference(float radius) {
         float circumference = PI * 2 * radius;
         System.out.println("Circumference: " +circumference);
-    }
-
-    public static float getRadius() {
-        return radius;
-    }
-
-    public static void setRadius(float radius) {
-        Class.radius = radius;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Class.setRadius(3);
-        Class.area();
-        Class.circumference();
+        Class.area(5,6);
+        Class.circumference(5);
     }
 }
